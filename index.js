ab11a2afa5485cd89c32237c5d714a2983fbb93c
Ralph Cox <73188137+essexgit@users.noreply.github.com>
2023-02-17 22:26:41 +0000

    saving data to file, questions done , needs formatting to markdown

--- index.js
+++ index.js
@@ -1,30 +1,102 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const fs = require("fs");
+// const path = require('path');
+const inquirer = require("inquirer");
+// const generateMarkdown = require("./utils/generateMarkdown");
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
+// Array of objects containing type, name and question
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Project Title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Project description?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your github username"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter email address",
+        validate: function (email) {
+            // Regex mail check (return true if valid mail)
+            return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+        }
+    },
+    {
+        type: "confirm",
+        name: "toc",
+        message: "Would you like a table of contents?",
+        default: true
+    },
+    {
+        type: "input",
+        name: "installCmd",
+        message: "What command installs the project for the user?"
+    },
+    {
+        type: "input",
+        name: "runCmd",
+        message: "What command runs the project for the user?"
+    },
+    {
+        type: "input",
+        name: "testCmd",
+        message: "What command tests the project?"
+    },
+    {
+        type: "input",
+        name: "operate",
+        message: "How does the user operate the application when it is running?"
+    },
+    {
+        type: "list",
+        name: "licence",
+        message: "What licence is used for the application?",
+        choices: ["Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Do What The F*ck You Want To Public License", "Educational Community License v2.0", "Eclipse Public License 1.0", "Eclipse Public License 2.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License family", "GNU General Public License v2.0", "GNU General Public License v3.0", "GNU Lesser General Public License family", "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project Public License v1.3c", "Microsoft Public License", "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "PostgreSQL License", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License"],
+        filter(val) {
+            return val;
         },
-        {
-            type: 'checkbox',
-            message: 'What languages do you know?',
-            name: 'stack',
-            choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
-        },
-        {
-            type: 'list',
-            message: 'What is your preferred method of communication?',
-            name: 'contact',
-            choices: ['email', 'phone', 'telekinesis'],
-        },
-    ])
-    .then((data) => {
-        const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
 
-        fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-            err ? console.log(err) : console.log('Success!')
-        );
+
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Who else has contributed and what did they do?"
+    },
+    // {
+    //     type: "input",
+    //     name: "fileName",
+    //     message: "Would you like the title 'ReadMe.md'? or would you like a different file name?"
+    // },
+    // {
+    //     type: "input",
+    //     name: "fileConflict",
+    //     message: "A readme file already exists in this directory, would you like to create a new directory, overwrite the existing file or cancel the operation?"
+    // }
+];
+
+// function to write README file
+function writeToFile(fileName = "ReadMe.md", text) {
+    fs.writeFile(fileName, text, (err) =>
+        err ? console.log(err) : console.log('Success!')
+    );
+}
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        // generateMarkdown(answers);
+        const text = (Object.values(answers)).toString();
+        console.log(text);
+        writeToFile("ReadMe.md", text);
     });
+}
+init();
