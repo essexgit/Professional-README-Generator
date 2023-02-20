29a1c6b86f20a1b58f9d726ef451600dc2ad471e
Ralph Cox <73188137+essexgit@users.noreply.github.com>
2023-02-20 09:25:58 +0000

    Badge and Readme todo

--- generated/README.md
+++ generated/README.md
@@ -1,9 +1,27 @@
-# thaat
-#### sdfgh
-## sdfag
-### sadf@swaedga.sagdf
-### asd
-### sadf
-### safda
-#### Creative Commons Attribution 4.0
-#### asdfga
+# Title: that
+[![License: LaTeX Project Public License v1.3c](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Table of Contents
+
+  [Description](#gdfg)
+
+  [Contact](#dfgs)
+
+  [How to:
+
+  - [Install](#dfh)
+
+  - [Run](#fdhgs)
+
+  - [Operate](#dffg)
+
+  [Licence](#LaTeX Project Public License v1.3c)
+
+  [Contributions](#sdgs)
+#### Description: gdfg
+## Github Username: dfgs
+### Email Address: dfg@sdfgh.dskgh
+### How to install: dfh
+### How to start: fdhgs
+### How to use: dffg
+#### Licence: LaTeX Project Public License v1.3c
+#### Notable contributions: sdgs
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,25 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  let line1 = `# ${data.title}\n`;
-  let line2 = `#### ${data.description}\n`;
-  let line3 = `## ${data.github}\n`;
-  let line4 = `### ${data.email}\n`;
-  let line6 = `### ${data.installCmd}\n`;
-  let line7 = `### ${data.runCmd}\n`;
-  let line8 = `### ${data.operate}\n`;
-  let line9 = `#### ${data.licence}\n`;
-  let line10 = `#### ${data.contribution}\n`;
-  let func = line1 + line2 + line3 + line4 + line6 + line7 + line8 + line9 + line10;
+  let line1 = `# Title: ${data.title}\n`;
+  let licenceBadge = `[![License: ${data.licence}](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n`;
+  let toc = `## Table of Contents\n
+  [Description](#${data.description})\n
+  [Contact](#${data.github})\n
+  ## How to:\n
+  - [Install](#${data.installCmd})\n
+  - [Run](#${data.runCmd})\n
+  - [Operate](#${data.operate})\n
+  [Licence](#${data.licence})\n
+  [Contributions](#${data.contribution})\n`;
+  let line2 = `#### Description: ${data.description}\n`;
+  let line3 = `## Github Username: ${data.github}\n`;
+  let line4 = `### Email Address: ${data.email}\n`;
+  let line6 = `### How to install: ${data.installCmd}\n`;
+  let line7 = `### How to start: ${data.runCmd}\n`;
+  let line8 = `### How to use: ${data.operate}\n`;
+  let line9 = `#### Licence: ${data.licence}\n`;
+  let line10 = `#### Notable contributions: ${data.contribution}\n`;
+  let func = line1 + licenceBadge + toc + line2 + line3 + line4 + line6 + line7 + line8 + line9 + line10;
   return func;
 }
 
